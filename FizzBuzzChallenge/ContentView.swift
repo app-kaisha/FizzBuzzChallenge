6d643f78d0e9edfabf6018b3beb9a87b0e64817b
app-kaisha <app.kaihatsusha@gmail.com>
2025-12-30 13:22:33 +0000

    Switch Statement solution

--- FizzBuzzChallenge/ContentView.swift
+++ FizzBuzzChallenge/ContentView.swift
@@ -15,7 +15,8 @@ struct ContentView: View {
             VStack {
                 ForEach(1..<101) { number in
                     //Text("\(getString(number: number))")
-                    Text("\(singleString(number: number))")
+//                    Text("\(singleString(number: number))")
+                    Text("\(switchString(number: number))")
                 }
             }
         }
@@ -37,6 +38,21 @@ struct ContentView: View {
     
     func singleString(number: Int) -> String{
         return "\(number%3 == 0 ? "Fizz" : "")\(number%5 == 0 ? "Buzz" : "")\(number%3 != 0 && number%5 != 0 ? "\(number)" : "")"
+    }
+    
+    func switchString(number: Int) -> String {
+        let tuple = (number % 3, number % 5)
+        switch tuple {
+        case(0,0):
+            return "FizzBuzz"
+        case(_, 0):
+            return "Buzz"
+        case(0,_):
+            return "Fizz"
+        default:
+            return "\(number)"
+        
+        }
     }
 }
 
