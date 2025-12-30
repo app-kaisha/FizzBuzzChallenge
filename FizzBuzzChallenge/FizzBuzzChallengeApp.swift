201e2475097c7dddcf74c7c3b380f33ee63c8588
app-kaisha <app.kaihatsusha@gmail.com>
2025-12-30 12:51:56 +0000

    Initial Commit

---
+++ FizzBuzzChallenge/FizzBuzzChallengeApp.swift
@@ -0,0 +1,18 @@
+//
+//  FizzBuzzChallengeApp.swift
+//  FizzBuzzChallenge
+//
+//  Created by app-kaihatsusha on 30/12/2025.
+//  Copyright © 2025 app-kaihatsusha. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct FizzBuzzChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
