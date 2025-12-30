81d521d043d4448be8ab076404214c11dbf829fa
app-kaisha <app.kaihatsusha@gmail.com>
2025-12-30 13:14:44 +0000

    Adding 'else' in if statments

--- FizzBuzzChallenge/ContentView.swift
+++ FizzBuzzChallenge/ContentView.swift
@@ -25,14 +25,13 @@ struct ContentView: View {
     func getString(number: Int) -> String {
         if (number % 3 == 0) && (number % 5 == 0) {
             return "FizzBuzz"
-        }
-        if number % 3 == 0 {
+        } else if number % 3 == 0 {
             return "Fizz"
-        }
-        if number % 5 == 0 {
+        } else if number % 5 == 0 {
             return "Buzz"
+        } else {
+            return "\(number)"
         }
-        return "\(number)"
     }
 }
 
