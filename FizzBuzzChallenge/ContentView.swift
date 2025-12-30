48f6c2f9843d8b7a5e3f2306d3e8a29feae39aa7
app-kaisha <app.kaihatsusha@gmail.com>
2025-12-30 13:18:23 +0000

    Single line nexts ternary solution

--- FizzBuzzChallenge/ContentView.swift
+++ FizzBuzzChallenge/ContentView.swift
@@ -14,7 +14,8 @@ struct ContentView: View {
         ScrollView {
             VStack {
                 ForEach(1..<101) { number in
-                    Text("\(getString(number: number))")
+                    //Text("\(getString(number: number))")
+                    Text("\(singleString(number: number))")
                 }
             }
         }
@@ -32,6 +33,10 @@ struct ContentView: View {
         } else {
             return "\(number)"
         }
+    }
+    
+    func singleString(number: Int) -> String{
+        return "\(number%3 == 0 ? "Fizz" : "")\(number%5 == 0 ? "Buzz" : "")\(number%3 != 0 && number%5 != 0 ? "\(number)" : "")"
     }
 }
 
